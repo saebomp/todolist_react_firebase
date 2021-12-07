34d252efdb72c49ee2b8de7fce3f9f9d4200065b
SAEBOMPAK <saebompak@gmail.com>
2021-12-06 19:32:33 -0800

    add firebase config

--- src/App.js
+++ src/App.js
@@ -4,22 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      dd
     </div>
   );
 }
 
 export default App;
+
+
+// https://www.youtube.com/watch?v=jCY6DH8F4oc   11:29
+// https://console.firebase.google.com/u/3/project/todolist-react-firebase-769a3/firestore/data/~2F
---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyDmlKTY33Ua4n6hI3UTvh4SOCJlXGTbZ8w",
+    authDomain: "todolist-react-firebase-769a3.firebaseapp.com",
+    projectId: "todolist-react-firebase-769a3",
+    storageBucket: "todolist-react-firebase-769a3.appspot.com",
+    messagingSenderId: "767367382385",
+    appId: "1:767367382385:web:e934dc3ef38760171fa2d0",
+    measurementId: "G-GQT8Q4YBS7"
+  };
+
+  const app = initializeApp(firebaseConfig);
