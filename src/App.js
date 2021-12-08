42a18de8243b39440c28fae9dab47c7a5a3f9c94
SAEBOMPAK <saebompak@gmail.com>
2021-12-07 20:50:01 -0800

    add updateUser

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import {useState, useEffect} from 'react'
 import './App.css';
 import {db} from './firebase-config'
-import {collection, getDocs, addDoc} from 'firebase/firestore'
+import {collection, getDocs, addDoc, updateDoc, doc} from 'firebase/firestore'
 
 function App() {
   const [newName, setNewName] = useState('')
   const [newAge, setNewAge] = useState(0)
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "users")
+  //usersCollectionRef : reference the whole collection
 
   const createUser =  async () => {
-    await addDoc(usersCollectionRef, {name:newName, age:newAge})
+    await addDoc(usersCollectionRef, { name:newName, age:Number(newAge) })
+  }
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, 'user', id)
+    const newFields = {age : age+1}
+    await updateDoc(userDoc, newFields)
   }
 
   useEffect(()=> {
@@ -41,6 +48,7 @@ function App() {
           <div>
             <div>{user.name}</div>
             <div>{user.age}</div>
+            <button onClick={() => {updateUser(user.id, user.age)}}>Increase Age</button>
           </div>
         )
       })}
